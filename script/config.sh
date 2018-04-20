e1069440e9da7326867885a38877189cb899875a
daniel1147 <sxn91401@gmail.com>
2018-04-20 16:09:30 +0000

    add config.sh

---
+++ script/config.sh
@@ -0,0 +1,7 @@
+# WEB="/home/sxn91401/JAVA/codility/src/main/java"
+WEB="/home/sxn91401/gradlePractice/build/reports/tests/test"
+RESTART_POLICY="--restart always"
+EXPORT_PORT="-p 11147:80"
+CONTAINER_NAME="--name web"
+MOUNT_CONFIG="-v /home/sxn91401/web/config/default.conf:/etc/nginx/conf.d/default.conf"
+MOUNT_DIR="-v ${WEB}:/usr/share/nginx/html:ro"
